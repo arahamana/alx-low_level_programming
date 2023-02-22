e4d2bce867bed74f26b5f5d4b01d9441a073d5af
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-22 14:56:29 -0800

    checking lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  * followed by a newline 
  */
+
 void print_alphabet_x10(void)
 {
 	char ch;
+
 	int b;
+
 	b = 0;
+
 	while (b < 10)
 	{
 		ch = 'a';
-
 		while (ch <= 'z')
 		{
 			_putchar(ch);
