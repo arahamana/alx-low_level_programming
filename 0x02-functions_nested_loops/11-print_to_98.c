0c12ed1c38f92304b0df93654c747dbcd86209a9
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-22 17:36:21 -0800

    midnight push

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_to_98.c - list all natural numbers from n to 98
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i <= 98; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+		return (0);
+}
