e2d19b0a5b323203267e955c253dd5aacb5f571f
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-23 15:16:41 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
