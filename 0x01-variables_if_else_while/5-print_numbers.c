3296941d281f2c5849572030d610d1bed854ab83
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-26 07:25:30 -0800

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints single numbers in base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
