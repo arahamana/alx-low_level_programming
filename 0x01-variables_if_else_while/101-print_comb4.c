7421ed15e5352b71600c273a010e5b3aaac94fbc
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-26 12:56:00 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,6 +11,8 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
+		for (m = 49; m < 58; m++;)
+		{
 		for (l = 50; l < 58; l++)
 		{
 			if (l > m && m > n)
@@ -25,6 +27,7 @@ int main(void)
 				}
 			}
 		}
+	}
 	}
 	putchar('\n');
 	return (0);
