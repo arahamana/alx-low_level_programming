2e1679c4c889ac08e61e6d30610ff0a8b9daa4fc
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-27 04:13:41 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ *
+ * @s: string to reverse
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s([i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
