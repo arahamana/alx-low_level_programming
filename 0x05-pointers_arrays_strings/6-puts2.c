32b4471ac645e7572bb527f141f02bf28f982bee
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-27 05:03:44 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @s: string to print
+ * Return: Always 0
+ */
+int puts2(char *s)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
