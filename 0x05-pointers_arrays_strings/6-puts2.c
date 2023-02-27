209b96822113922b079e9fe8f8e6a211c2069273
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-27 09:09:28 -0800

    print char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,18 @@
 /**
  * puts2 - prints every other character of a string
  *
- * @s: string to print
+ * @str: string to print
  * Return: Always 0
  */
-int puts2(char *s)
+int puts2(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 		i++;
 	}
