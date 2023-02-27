503cfea54e3a861889859d7a5b015de67d6ee154
Abdul Rahaman Jabar <arahamana2011@gmail.com>
2023-02-27 10:11:03 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements of an array of integers
+ * @n: numbee of integers to print
+ * @a: pointer
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(",");
+	}
+	printf("\n");
+}
