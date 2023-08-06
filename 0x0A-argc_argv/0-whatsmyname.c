6a252fecf9ac157c5667bf9867c17166d5cdbb49
Abdul Rahaman Abdul Jabar <arahamana2011@gmail.com>
2023-08-06 14:16:11 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /*
- *main - prints name of program, followed by a new line
+ *whatsmymame - prints name of program, followed by a new line
  * argc - argument count
  * argv - array of arguments
  *
@@ -11,7 +11,7 @@
 
 int main(int argc _attribute_((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", *argv[0]);
 
 	return (0);
 }
