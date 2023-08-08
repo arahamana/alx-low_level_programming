238bfb50dc9356074eec52882d2b8fa2fa3e76c3
Abdul Rahaman Abdul Jabar <arahamana2011@gmail.com>
2023-08-08 02:20:46 -0700

    upate

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /*
- * main() - prints number of arguments passed onto it
- * argc - argument count
- * argv[] - argument vector or string of arrays
- * char - character
- * return - always 0 on success
+ * main - prints number of arguments passed onto it
+ * argc:  argument count
+ * argv: argument vector or string of arrays
+ * char: - character
+ * return: always 0 on success
  */
 
 int main(int argc, char *argv[])
