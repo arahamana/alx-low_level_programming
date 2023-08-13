60a6ae48fcf720416f44d238ac755608caf42c6a
Abdul Rahaman Abdul Jabar <arahamana2011@gmail.com>
2023-08-13 09:40:39 -0700

    task push

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * _strlen_recursion - prints length of a string.
+ * @s - the string to be counted.
+ * return - the string length.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int stringLentgth = 0;
+
+	if (*s)
+	{
+		stringLength++;
+		stringLength += _strlen_recursion(s + 1);
+	}
+	return (stringLength);
+}
