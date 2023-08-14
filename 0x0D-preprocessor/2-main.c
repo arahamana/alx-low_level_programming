16f2aed8cacb8df9b35e4190f9960326c1d9498a
Abdul Rahaman Abdul Jabar <arahamana2011@gmail.com>
2023-08-14 14:45:56 -0700

    all task pushed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * __FILE__ - predefined macro that prints name of file
+ * Return - always 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/Readme.md
@@ -0,0 +1,1 @@
+This directory contains completed task on preprocessor directives in pursuant of software engineering course with alx.
