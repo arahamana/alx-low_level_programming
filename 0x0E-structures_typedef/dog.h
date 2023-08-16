9dfaabcdb4366070e2bdfe7daa188e3b5c165139
Abdul Rahaman Abdul Jabar <arahamana2011@gmail.com>
2023-08-15 17:57:47 -0700

    chanhges

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,7 @@ struct dog
 /**
  * dog_info - typedef for struct dog
  */
-typedef struct dog dog_info;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
